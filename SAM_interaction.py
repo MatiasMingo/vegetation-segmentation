5be80094c74fe83bad1e4d6becba503d45be9072
Matías Mingo <mamingo@uc.cl>
2023-12-29 10:24:50 -0300

    feat: refactor

--- SAM_interaction.py
+++ SAM_interaction.py
@@ -1,32 +1,27 @@
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
 import cv2
-
 import sys
-sys.path.append("..")
-from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+#sys.path.append("..")
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 
-sam_checkpoint = "sam_vit_h_4b8939.pth"
-model_type = "vit_h"
+def load_model():
+    sam_checkpoint = "sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
+    #device = "cuda"
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    #sam.to(device=device)
+    mask_generator = SamAutomaticMaskGenerator(
+        model=sam,
+        points_per_side=32,
+        pred_iou_thresh=0.86,
+        stability_score_thresh=0.95,
+        crop_n_layers=1,
+        crop_n_points_downscale_factor=20,
+        min_mask_region_area=100
+    )
+    return mask_generator
 
-#device = "cuda"
-
-sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-#sam.to(device=device)
-
-mask_generator = SamAutomaticMaskGenerator(sam)
-image = cv2.imread('PLANTAS.tiff')
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-mask_generator_2 = SamAutomaticMaskGenerator(
-    model=sam,
-    points_per_side=32,
-    pred_iou_thresh=0.86,
-    stability_score_thresh=0.95,
-    crop_n_layers=1,
-    crop_n_points_downscale_factor=20,
-    min_mask_region_area=100,  # Requires open-cv to run post-processing
-)
-
-masks2 = mask_generator_2.generate(image)
+def predict_masks(mask_generator, geotiff_path):
+    image = cv2.imread(geotiff_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    masks = mask_generator.generate(image)
+    return masks
